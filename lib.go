16b2522c214b7078e61ee88d8a230962b3207cde
destinyhover <nikitos.21333@gmail.com>
2025-05-15 18:26:38 +0300

    Add godoc

--- lib.go
+++ lib.go
@@ -1,5 +1,10 @@
+// Пакет workspace_lib представляет утилиту Add
 package workspace_lib
 
+// AddNums принимает два экземпляра типа int и возвращает один int - сумму
+// Более подробные сведения можно найти на сайте [MathIsFun].
+//
+// [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
 func AddNums(a, b int) int {
 	return a + b
 }
