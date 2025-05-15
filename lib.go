dc5a955a72b4ab42bd1bd9f9609a99a589449445
destinyhover <nikitos.21333@gmail.com>
2025-05-15 18:48:28 +0300

    Add constrains

--- lib.go
+++ lib.go
@@ -1,10 +1,17 @@
 // Пакет workspace_lib представляет утилиту Add
 package workspace_lib
 
-// AddNums принимает два экземпляра типа int и возвращает один int - сумму
+import "golang.org/x/exp/constraints"
+
+// AddNums принимает два экземпляра типа int или float и возвращает один исходного типа - сумму
 // Более подробные сведения можно найти на сайте [MathIsFun].
 //
 // [MathIsFun]: https://www.mathsisfun.com/numbers/addition.html
-func AddNums(a, b int) int {
+func AddNums[T Number](a, b T) T {
 	return a + b
+}
+
+// Number интерфейс реализует два импортируемых типа Integer and Float
+type Number interface {
+	constraints.Integer | constraints.Float
 }
